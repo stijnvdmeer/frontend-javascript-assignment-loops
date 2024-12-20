d44e3016fff161cc1e5b51a4027e7500d67cfb81
stijnvdmeer <stijn08072002@outlook.com>
2024-12-20 10:50:41 +0100

    Loop Opdracht 4 Bonus

--- loops.js
+++ loops.js
@@ -106,5 +106,19 @@ for(i = 0; i <= 9; i++) {
 // etc.
 // ==========================================
 
+for(i = 0; i <= 100; i++) {
+    if(i % 5 === 0 && i % 3 === 0) {
+        console.log("FizzBuzz");
+    }
+    else if(i % 5 === 0) {
+        console.log("Buzz");
+    }
+    else if (i % 3 === 0) {
+        console.log("Fizz");
+    }
+    else {
+        console.log(i);
+    }
+}
 
 
